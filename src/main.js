87bb7afad9dbffe2606145fe59381fa7bd8770bf
hezhaoshun <373693643@qq.com>
2020-08-17 11:45:15 +0800

    postcss-pxtorem + lib-flexible 实现适配

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import { Button } from 'vant';
+import 'vant/lib/index.css';
+import 'lib-flexible';
+
+Vue.use(Button);
 
 Vue.config.productionTip = false
 
